b7acd0c4d09d5c23b782d2172fa46dad5ef33ef3
zhuanglx <zhlx012@163.com>
2017-08-11 11:08:55 +0800

    create workset
    create workset

---
+++ parent/dao/src/main/java/com/zhuanglx/demo/dao/App.java
@@ -0,0 +1,13 @@
+package com.zhuanglx.demo.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
